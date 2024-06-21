c00aeabdc950576a109d26a75a4ca121cf33878c
comet13579 <sonny3142@gmail.com>
2024-06-22 04:22:36 +0800

    raindrop function done for testing tap notes

--- main.c
+++ main.c
@@ -1,32 +1,33 @@
 #include "driver.h"
 
-int main(){
-    JOY_init();
-    OLED_init();
-    OLED_println("Hello World!");
+void display_raindrop(uint8_t xdotpos, uint8_t ydotpos){
+    uint8_t y, x;
+    for (y = 0; y < 8; y++){
+        JOY_OLED_data_start(y);
+        for (x = 0; x < 128; x++){
+        if (x == xdotpos && ydotpos / 8 == y){
+            JOY_OLED_send(0x01 << (ydotpos % 8));
+        } 
+        else{
+            JOY_OLED_send(0x00);
+        }
+    }
+    JOY_OLED_end();
+  }
 }
-
 
 //x horizontal y vertical
 void display_raining(uint8_t xpos){
     for (uint8_t i = 0; i < 64; i++){
         display_raindrop(xpos, i);
-        DLY_ms(16);
+        DLY_ms(100);
     }
 }
 
-void display_raindrop(uint8_t xpos, uint8_t ypos){
-    for (uint8_t y = 0; y < 8; y++){
-        JOY_OLED_data_start(y);
-        for (uint8_t x = 0; x < 128 ; x++){
-            if (x == xpos && y == ypos / 8){
-                JOY_OLED_send(0x80 >> (x % 8));
-            }
-            else{
-                JOY_OLED_send(0x00);
-            }
-        JOY_OLED_end();
-        }
-
-    }
+int main(){
+    JOY_init();
+    while (1){
+        OLED_init();
+        display_raining(64);
+    };
 }
