2074416d38d423c372c79af6ed44de06bb727739
comet13579 <sonny3142@gmail.com>
2024-06-23 00:10:40 +0800

    snake game init screen

--- main.c
+++ main.c
@@ -1,33 +1,46 @@
 #include "driver.h"
+#include "snake.h"
 
-void display_raindrop(uint8_t xdotpos, uint8_t ydotpos){
+;void game_init(){
+    JOY_init();
+    for (uint8_t i = 0;i < 128 ; i++){
+        gameboard[i] = '0';
+    }
+    gameboard[33] = 'h';
+    gameboard[43] = 'a';
+    gameboard[32] = 't';
+
+}
+
+uint8_t gameboard_to_hex(uint8_t x,uint8_t y){
+    uint8_t gameboard_index = y * 16 + x / 8;
+    if (gameboard[gameboard_index] == '0'){
+        return 0x00;
+    }
+    if (gameboard[gameboard_index] == 'a'){
+        return apple[x % 8];
+    }
+    if (gameboard[gameboard_index] == 'h'){
+        return rightSnakeHead[x % 8];
+    }
+    if (gameboard[gameboard_index] == 't'){
+        return leftSnakeTail[x % 8];
+    }
+    return 0x00;
+}
+
+void display(){ //x horizontal y vertical
     uint8_t y, x;
     for (y = 0; y < 8; y++){
         JOY_OLED_data_start(y);
         for (x = 0; x < 128; x++){
-        if (x == xdotpos && ydotpos / 8 == y){
-            JOY_OLED_send(0x01 << (ydotpos % 8));
-        } 
-        else{
-            JOY_OLED_send(0x00);
-        }
+            JOY_OLED_send(gameboard_to_hex(x,y));
     }
     JOY_OLED_end();
   }
 }
 
-//x horizontal y vertical
-void display_raining(uint8_t xpos){
-    for (uint8_t i = 0; i < 64; i++){
-        display_raindrop(xpos, i);
-        DLY_ms(100);
-    }
-}
-
 int main(){
-    JOY_init();
-    while (1){
-        OLED_init();
-        display_raining(64);
-    };
+    game_init();
+    display();
 }
