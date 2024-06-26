0bfb9e37518933eca254b6b35d93d36bc3d7c3be
Cheng Ho Ming, Eric <eric310@connect.hku.hk>
2024-06-26 16:43:35 +0800

    shiftImage (buggy)

--- music.c
+++ music.c
@@ -1,148 +1,7 @@
+#include "music.h"
+
 #include "driver.h"
-// based on https://github.com/robsoncouto/arduino-songs
-
-#define NOTE_B0 31
-#define NOTE_C1 33
-#define NOTE_CS1 35
-#define NOTE_D1 37
-#define NOTE_DS1 39
-#define NOTE_E1 41
-#define NOTE_F1 44
-#define NOTE_FS1 46
-#define NOTE_G1 49
-#define NOTE_GS1 52
-#define NOTE_A1 55
-#define NOTE_AS1 58
-#define NOTE_B1 62
-#define NOTE_C2 65
-#define NOTE_CS2 69
-#define NOTE_D2 73
-#define NOTE_DS2 78
-#define NOTE_E2 82
-#define NOTE_F2 87
-#define NOTE_FS2 93
-#define NOTE_G2 98
-#define NOTE_GS2 104
-#define NOTE_A2 110
-#define NOTE_AS2 117
-#define NOTE_B2 123
-#define NOTE_C3 131
-#define NOTE_CS3 139
-#define NOTE_D3 147
-#define NOTE_DS3 156
-#define NOTE_E3 165
-#define NOTE_F3 175
-#define NOTE_FS3 185
-#define NOTE_G3 196
-#define NOTE_GS3 208
-#define NOTE_A3 220
-#define NOTE_AS3 233
-#define NOTE_B3 247
-#define NOTE_C4 262
-#define NOTE_CS4 277
-#define NOTE_D4 294
-#define NOTE_DS4 311
-#define NOTE_E4 330
-#define NOTE_F4 349
-#define NOTE_FS4 370
-#define NOTE_G4 392
-#define NOTE_GS4 415
-#define NOTE_A4 440
-#define NOTE_AS4 466
-#define NOTE_B4 494
-#define NOTE_C5 523
-#define NOTE_CS5 554
-#define NOTE_D5 587
-#define NOTE_DS5 622
-#define NOTE_E5 659
-#define NOTE_F5 698
-#define NOTE_FS5 740
-#define NOTE_G5 784
-#define NOTE_GS5 831
-#define NOTE_A5 880
-#define NOTE_AS5 932
-#define NOTE_B5 988
-#define NOTE_C6 1047
-#define NOTE_CS6 1109
-#define NOTE_D6 1175
-#define NOTE_DS6 1245
-#define NOTE_E6 1319
-#define NOTE_F6 1397
-#define NOTE_FS6 1480
-#define NOTE_G6 1568
-#define NOTE_GS6 1661
-#define NOTE_A6 1760
-#define NOTE_AS6 1865
-#define NOTE_B6 1976
-#define NOTE_C7 2093
-#define NOTE_CS7 2217
-#define NOTE_D7 2349
-#define NOTE_DS7 2489
-#define NOTE_E7 2637
-#define NOTE_F7 2794
-#define NOTE_FS7 2960
-#define NOTE_G7 3136
-#define NOTE_GS7 3322
-#define NOTE_A7 3520
-#define NOTE_AS7 3729
-#define NOTE_B7 3951
-#define NOTE_C8 4186
-#define NOTE_CS8 4435
-#define NOTE_D8 4699
-#define NOTE_DS8 4978
-#define REST 0
-
-// notes of the moledy followed by the duration.
-// a 4 means a quarter note, 8 an eighteenth , 16 sixteenth, so on
-// !!negative numbers are used to represent dotted notes,
-// so -4 means a dotted quarter note, that is, a quarter plus an eighteenth!!
-int melody[] = {
-
-    // Greensleeves
-    // Score available at https://musescore.com/user/168402/scores/1396946
-    // Alexander Trompoukis
-
-    NOTE_G4, 8, // 1
-    NOTE_AS4, 4, NOTE_C5, 8, NOTE_D5, -8, NOTE_DS5, 16, NOTE_D5, 8, NOTE_C5, 4, NOTE_A4,
-    8, NOTE_F4, -8, NOTE_G4, 16, NOTE_A4, 8, NOTE_AS4, 4, NOTE_G4, 8, NOTE_G4, -8,
-    NOTE_FS4, 16, NOTE_G4, 8, NOTE_A4, 4, NOTE_FS4, 8, NOTE_D4, 4, NOTE_G4, 8,
-
-    NOTE_AS4, 4, NOTE_C5, 8, NOTE_D5, -8, NOTE_DS5, 16, NOTE_D5, 8, // 6
-    NOTE_C5, 4, NOTE_A4, 8, NOTE_F4, -8, NOTE_G4, 16, NOTE_A4, 8, NOTE_AS4, -8, NOTE_A4,
-    16, NOTE_G4, 8, NOTE_FS4, -8, NOTE_E4, 16, NOTE_FS4, 8, NOTE_G4, -2, NOTE_F5, 2,
-    NOTE_E5, 16, NOTE_D5, 8,
-
-    NOTE_C5, 4, NOTE_A4, 8, NOTE_F4, -8, NOTE_G4, 16, NOTE_A4, 8, // 11
-    NOTE_AS4, 4, NOTE_G4, 8, NOTE_G4, -8, NOTE_FS4, 16, NOTE_G4, 8, NOTE_A4, 4,
-    NOTE_FS4, 8, NOTE_D4, 04, NOTE_F5, 2, NOTE_E5, 16, NOTE_D5, 8, NOTE_C5, 4, NOTE_A4,
-    8, NOTE_F4, -8, NOTE_G4, 16, NOTE_A4, 8,
-
-    NOTE_AS4, -8, NOTE_A4, 16, NOTE_G4, 8, NOTE_FS4, -8, NOTE_E4, 16, NOTE_FS4,
-    8, // 16
-    NOTE_G4, -2,
-
-    // repeats from the beginning
-
-    NOTE_G4, 8, // 1
-    NOTE_AS4, 4, NOTE_C5, 8, NOTE_D5, -8, NOTE_DS5, 16, NOTE_D5, 8, NOTE_C5, 4, NOTE_A4,
-    8, NOTE_F4, -8, NOTE_G4, 16, NOTE_A4, 8, NOTE_AS4, 4, NOTE_G4, 8, NOTE_G4, -8,
-    NOTE_FS4, 16, NOTE_G4, 8, NOTE_A4, 4, NOTE_FS4, 8, NOTE_D4, 4, NOTE_G4, 8,
-
-    NOTE_AS4, 4, NOTE_C5, 8, NOTE_D5, -8, NOTE_DS5, 16, NOTE_D5, 8, // 6
-    NOTE_C5, 4, NOTE_A4, 8, NOTE_F4, -8, NOTE_G4, 16, NOTE_A4, 8, NOTE_AS4, -8, NOTE_A4,
-    16, NOTE_G4, 8, NOTE_FS4, -8, NOTE_E4, 16, NOTE_FS4, 8, NOTE_G4, -2, NOTE_F5, 2,
-    NOTE_E5, 16, NOTE_D5, 8,
-
-    NOTE_C5, 4, NOTE_A4, 8, NOTE_F4, -8, NOTE_G4, 16, NOTE_A4, 8, // 11
-    NOTE_AS4, 4, NOTE_G4, 8, NOTE_G4, -8, NOTE_FS4, 16, NOTE_G4, 8, NOTE_A4, 4,
-    NOTE_FS4, 8, NOTE_D4, 4, NOTE_F5, 2, NOTE_E5, 16, NOTE_D5, 8, NOTE_C5, 4, NOTE_A4,
-    8, NOTE_F4, -8, NOTE_G4, 16, NOTE_A4, 8,
-
-    NOTE_AS4, -8, NOTE_A4, 16, NOTE_G4, 8, NOTE_FS4, -8, NOTE_E4, 16, NOTE_FS4,
-    8, // 16
-    NOTE_G4, -2
-
-};
+#include "image.c"
 
 // convert int to char[]
 void intToChars(int num, char * str) {
@@ -201,25 +60,79 @@ void playMusic() { // sizeof gives the number of bytes, each int value is
         intToChars(noteDuration, noteDurationChar);
         OLED_println(noteDurationChar);
         JOY_sound(melody[thisNote], noteDuration);
-        JOY_DLY_ms(10);
     }
 }
 
-struct note {
-    int frequency;
-    float wholenote_count;
-};
-struct note notes[] = {{400, 0.00}, {500, 0.00}, {400, 0.00}, {400, 0.00}, {400, 0.00},
-    {500, 0.00}, {500, 0.00}, {500, 0.00}, {400, 0.00}, {400, 0.00}};
-
 void playNotes() {
-    for (int i = 0; i < sizeof(notes) / sizeof(notes[0]); i++) {
+    for (uint8_t i = 0; i < sizeof(notes) / sizeof(notes[0]); i++) {
         JOY_sound(notes[i].frequency, 10);
+    }
+}
+
+void display(const uint8_t pixelArray[], uint16_t size) {
+    OLED_fill(0x00);
+    uint8_t y, x;
+    for (y = 0; y < 8; y++) {
+        JOY_OLED_data_start(y);
+        for (x = 0; x < size / 8; x++) {
+            JOY_OLED_send(pixelArray[y * 128 + x]);
+        }
+        JOY_OLED_end();
+    }
+}
+
+void shiftImage(offset_t offsetCoord, const uint8_t oldImage[], uint8_t newImage[],
+    ScreenCoord coords) {
+    // Clear the new image
+    for (int i = 0; i < coords.x * coords.y / 8; i++) {
+        newImage[i] = 0;
+    }
+
+    // Shift the image based on the offset coordinates
+    for (int y = 0; y < coords.y; y++) {
+        for (int x = 0; x < coords.x; x++) {
+            int newX = x + offsetCoord.x;
+            int newY = y + offsetCoord.y;
+
+            if (newX >= 0 && newX < coords.x && newY >= 0 && newY < coords.y) {
+                int oldByteIndex = (y * coords.x + x) / 8;
+                int oldBitIndex = x % 8;
+                int newByteIndex = (newY * coords.x + newX) / 8;
+                int newBitIndex = newX % 8;
+
+                // Extract the bit from the old image
+                uint8_t bit = (oldImage[oldByteIndex] >> oldBitIndex) & 1;
+
+                // Set the bit in the new image
+                newImage[newByteIndex] |= (bit << newBitIndex);
+            }
+        }
     }
 }
 
 int main() {
     JOY_init();
+    OLED_fill(0x00);
+
+    // copy the image data to a new array
+    uint8_t image_data_copy[1024];
+    for (int i = 0; i < 1024; i++) {
+        image_data_copy[i] = image_data[i];
+    }
+    for (int i = 0; i < 100; i++) {
+        display(image_data_copy, 1024);
+        DLY_ms(300);
+        shiftImage(
+            (offset_t){3, 3}, image_data, image_data_copy, (ScreenCoord){128, 64});
+        // negateImage(image_data_copy, 1024);
+        display(image_data_copy, 1024);
+        DLY_ms(300);
+        shiftImage(
+            (offset_t){-3, 3}, image_data, image_data_copy, (ScreenCoord){128, 64});
+        // negateImage(image_data_copy, 1024);
+        display(image_data_copy, 1024);
+        DLY_ms(300);
+    }
     OLED_fill(0x00);
     OLED_println("Press the right button to start the game");
     while (1) {
